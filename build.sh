8318e2b01776915a3e428450559bc377c49eb5fa
George Hotz <george@comma.ai>
2018-04-18 18:17:25 -0700

    build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+download_to_directory() {
+  mkdir -p $1
+  curl $2 | tar xvfJ - -C $1 --strip-components=1
+}
+
+#download_to_directory llvm http://releases.llvm.org/6.0.0/llvm-6.0.0.src.tar.xz
+#download_to_directory llvm/tools/clang http://releases.llvm.org/6.0.0/cfe-6.0.0.src.tar.xz
+
+# output directory
+mkdir usr
+
+# build directory
+mkdir build
+cmake ../llvm
+
+
+
+
