c5f46c00a3f57e1bc982be594aabf3a75108e195
George Hotz <george@comma.ai>
2018-04-19 08:45:36 -0700

    go

--- go.sh
+++ go.sh
@@ -5,16 +5,15 @@ download_to_directory() {
   curl $2 | tar xvfJ - -C $1 --strip-components=1
 }
 
-#download_to_directory llvm http://releases.llvm.org/6.0.0/llvm-6.0.0.src.tar.xz
-#download_to_directory llvm/tools/clang http://releases.llvm.org/6.0.0/cfe-6.0.0.src.tar.xz
+download_to_directory llvm http://releases.llvm.org/6.0.0/llvm-6.0.0.src.tar.xz
+download_to_directory llvm/tools/clang http://releases.llvm.org/6.0.0/cfe-6.0.0.src.tar.xz
 
 # output directory
 mkdir -p usr
 
 # build directory
 mkdir -p build
+cd build
 cmake ../llvm
-
-
-
+make -j
 
