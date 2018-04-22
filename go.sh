a68da5aac469ec36c2553b7395975ee423de9b26
George Hotz <george@comma.ai>
2018-04-22 11:40:41 -0700

    build mclinker works

--- go.sh
+++ go.sh
@@ -23,31 +23,58 @@ build_binutils() {
   make install
 }
 
-#build_binutils
-#exit
+build_llvm() {
+  pushd .
+  # the basic three for llvm/clang
+  # switch to 4.0.0 for MCLinker
+  download_to_directory llvm http://releases.llvm.org/4.0.0/llvm-4.0.0.src.tar.xz
+  download_to_directory llvm/tools/clang http://releases.llvm.org/4.0.0/cfe-4.0.0.src.tar.xz
+  download_to_directory llvm/projects/compiler-rt http://releases.llvm.org/4.0.0/compiler-rt-4.0.0.src.tar.xz
 
-# the basic three for llvm/clang
-download_to_directory llvm http://releases.llvm.org/6.0.0/llvm-6.0.0.src.tar.xz
-download_to_directory llvm/tools/clang http://releases.llvm.org/6.0.0/cfe-6.0.0.src.tar.xz
-download_to_directory llvm/projects/compiler-rt http://releases.llvm.org/6.0.0/compiler-rt-6.0.0.src.tar.xz
+  # llvm linker (doesn't support Hexagon?)
+  #download_to_directory llvm/tools/lld http://releases.llvm.org/6.0.0/lld-6.0.0.src.tar.xz
 
-# llvm linker (doesn't support Hexagon?)
-#download_to_directory llvm/tools/lld http://releases.llvm.org/6.0.0/lld-6.0.0.src.tar.xz
+  # no C++ support yet
+  #download_to_directory llvm/projects/libcxx http://releases.llvm.org/6.0.0/libcxx-6.0.0.src.tar.xz
+  #download_to_directory llvm/projects/libcxxabi http://releases.llvm.org/6.0.0/libcxxabi-6.0.0.src.tar.xz
+  #download_to_directory llvm/projects/libunwind http://releases.llvm.org/6.0.0/libunwind-6.0.0.src.tar.xz
 
-# no C++ support yet
-#download_to_directory llvm/projects/libcxx http://releases.llvm.org/6.0.0/libcxx-6.0.0.src.tar.xz
-#download_to_directory llvm/projects/libcxxabi http://releases.llvm.org/6.0.0/libcxxabi-6.0.0.src.tar.xz
-#download_to_directory llvm/projects/libunwind http://releases.llvm.org/6.0.0/libunwind-6.0.0.src.tar.xz
+  # build directory
+  mkdir -p build-llvm
+  cd build-llvm
 
-# build directory
-mkdir -p build-llvm
-cd build-llvm
+  # due to the hexagon-link issue, this doesn't work...
+  #cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PWD/../usr -DLLVM_DEFAULT_TARGET_TRIPLE=hexagon-unknown-linux-gnu -DLLVM_TARGET_ARCH=hexagon-unknown-linux-gnu -DLLVM_TARGETS_TO_BUILD=Hexagon -DLLVM_BUILD_EXTERNAL_COMPILER_RT=ON ../llvm
 
-# due to the hexagon-link issue, this doesn't work...
-#cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PWD/../usr -DLLVM_DEFAULT_TARGET_TRIPLE=hexagon-unknown-linux-gnu -DLLVM_TARGET_ARCH=hexagon-unknown-linux-gnu -DLLVM_TARGETS_TO_BUILD=Hexagon -DLLVM_BUILD_EXTERNAL_COMPILER_RT=ON ../llvm
+  cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DLLVM_DEFAULT_TARGET_TRIPLE=hexagon-unknown--elf -DLLVM_TARGET_ARCH=hexagon-unknown--elf -DLLVM_TARGETS_TO_BUILD=Hexagon ../llvm
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DLLVM_DEFAULT_TARGET_TRIPLE=hexagon-unknown--elf -DLLVM_TARGET_ARCH=hexagon-unknown--elf -DLLVM_TARGETS_TO_BUILD=Hexagon ../llvm
+  make -j12
+  make install
+  popd
+}
 
-make -j12
-make install
+build_mclinker() {
+  pushd .
+  git clone git@github.com:geohot/mclinker.git --depth=1
+
+  # autogen
+  cd mclinker
+  ./autogen.sh
+  cd ../
+
+  mkdir -p build-mclinker
+  cd build-mclinker
+  ../mclinker/configure --enable-targets=hexagon --prefix=$PREFIX
+  make -j12
+  make install
+
+  # link
+  cd $PREFIX/bin
+  ln -s ld.mcld hexagon-link
+  popd
+}
+
+#build_llvm
+build_mclinker
+
 
--- test/build.sh
+++ test/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 export PATH=$PWD/../usr/bin/:$PATH
-clang -mv60 -mhvx -mhvx-length=128B -march=hexagon -mcpu=hexagonv60 -v -fpic -c main.c
-hexagon-ld -A hexagon main.o
+clang -mv60 -mhvx-double -march=hexagon -mcpu=hexagonv60 -v -fpic main.c
 
