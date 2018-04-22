c5edbec42adb99c35c8142d508b783c947a76a26
George Hotz <george@comma.ai>
2018-04-22 11:42:00 -0700

    sweet, test works

--- test/build.sh
+++ test/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PATH=$PWD/../usr/bin/:$PATH
-clang -mv60 -mhvx-double -march=hexagon -mcpu=hexagonv60 -v -fpic main.c
+clang -mv60 -mhvx-double -march=hexagon -mcpu=hexagonv60 -v -fpic -shared -o test.so main.c
 
