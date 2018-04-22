bddc01f54d0e876d03117cb21f3154d9924f69fa
George Hotz <george@comma.ai>
2018-04-22 09:35:38 -0700

    better

--- go.sh
+++ go.sh
@@ -18,12 +18,15 @@ build_binutils() {
   make install
 }
 
+#build_binutils
+#exit
+
 # the basic three for llvm/clang
 download_to_directory llvm http://releases.llvm.org/6.0.0/llvm-6.0.0.src.tar.xz
 download_to_directory llvm/tools/clang http://releases.llvm.org/6.0.0/cfe-6.0.0.src.tar.xz
 download_to_directory llvm/projects/compiler-rt http://releases.llvm.org/6.0.0/compiler-rt-6.0.0.src.tar.xz
 
-# still using binutils for 'hexagon-link'
+# llvm linker (doesn't support Hexagon?)
 #download_to_directory llvm/tools/lld http://releases.llvm.org/6.0.0/lld-6.0.0.src.tar.xz
 
 # no C++ support yet
@@ -32,13 +35,13 @@ download_to_directory llvm/projects/compiler-rt http://releases.llvm.org/6.0.0/c
 #download_to_directory llvm/projects/libunwind http://releases.llvm.org/6.0.0/libunwind-6.0.0.src.tar.xz
 
 # build directory
-mkdir -p build
-cd build
+mkdir -p build-llvm
+cd build-llvm
 
-# due to the hexagon link issue, this doesn't work...
+# due to the hexagon-link issue, this doesn't work...
 #cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PWD/../usr -DLLVM_DEFAULT_TARGET_TRIPLE=hexagon-unknown-linux-gnu -DLLVM_TARGET_ARCH=hexagon-unknown-linux-gnu -DLLVM_TARGETS_TO_BUILD=Hexagon -DLLVM_BUILD_EXTERNAL_COMPILER_RT=ON ../llvm
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PWD/../usr -DLLVM_DEFAULT_TARGET_TRIPLE=hexagon-unknown-linux-gnu -DLLVM_TARGET_ARCH=hexagon-unknown-linux-gnu -DLLVM_TARGETS_TO_BUILD=Hexagon ../llvm
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PWD/../usr -DLLVM_DEFAULT_TARGET_TRIPLE=hexagon-unknown--elf -DLLVM_TARGET_ARCH=hexagon-unknown--elf -DLLVM_TARGETS_TO_BUILD=Hexagon ../llvm
 
 make -j12
 make install
